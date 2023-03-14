060025268e42222477df2dbe6fd8227cf9baaf88
hariom-ojha23 <hariomojha2301@gmail.com>
2023-03-14 15:11:19 +0530

    changing origin

--- backend/index.js
+++ backend/index.js
@@ -26,14 +26,14 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://codesplit-frontend.onrender.com',
   },
 })
 
 app.use(express.json())
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://codesplit-frontend.onrender.com',
   })
 )
 
