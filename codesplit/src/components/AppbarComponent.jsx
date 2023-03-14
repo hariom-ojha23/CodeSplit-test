166fabb4a37cfcafe9273fd451f84a3f0b150f5b
hariom-ojha23 <hariomojha2301@gmail.com>
2023-03-14 19:16:13 +0530

    removing appbar links for now

--- codesplit/src/components/AppbarComponent.jsx
+++ codesplit/src/components/AppbarComponent.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 const AppbarComponent = () => {
   return (
     <div className='homeAppbarContainer'>
-        <div className='appbarInnerContainer'>
-          <img
-            className='appbar-logo'
-            src='/codesplit.png'
-            alt='codesplit-logo'
-          />
-          <ul className='appbar-list'>
-            <li className='appbar-list-item'>
+      <div className='appbarInnerContainer'>
+        <img
+          className='appbar-logo'
+          src='/codesplit.png'
+          alt='codesplit-logo'
+        />
+        <ul className='appbar-list'>
+          {/* <li className='appbar-list-item'>
               <Link className='appbar-item-link' to='/about'>
                 About Us
               </Link>
@@ -20,10 +20,10 @@ const AppbarComponent = () => {
               <Link className='appbar-item-link' to='/login'>
                 Login
               </Link>
-            </li>
-          </ul>
-        </div>
+            </li> */}
+        </ul>
       </div>
+    </div>
   )
 }
 
